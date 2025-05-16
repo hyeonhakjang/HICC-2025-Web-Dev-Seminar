0fc4dbdbf72dadad87e35eec872ce1cf0379eae8
장현학\gusgk <gusgkr785@gmail.com>
2025-05-17 06:58:24 +0900

    .

---
+++ hicc_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from .models import Question, Answer
+
+def question_detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    answers = Answer.objects.filter(question=question)
+    return render(request, 'question_detail_ssr.html', {
+        'question': question,
+        'answers': answers,
+    })
